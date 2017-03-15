a62e4f8760c1a7211e2afd687429b2f376c83de4
dfreemanRIIS <dfreeman@riis.com>
2017-03-15 14:26:03 -0400

    Set up firebase DB

---
+++ Jive Factory/Bridge.m
@@ -0,0 +1,10 @@
+//
+//  Bridge.m
+//  Jive Factory
+//
+//  Created by Donald Freeman on 3/15/17.
+//  Copyright © 2017 Noble Desktop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Firebase/Firebase.h>
---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'Jive Factory' do
+    pod 'Firebase/Core'
+    pod 'Firebase/Database'
+end
